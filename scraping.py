6fa68fe9b3a7333784ada35e7e6b68b6065e612b
wassim3884 <Wassim_Rahal@outlook.com>
2024-11-17 22:04:42 +0100

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,1 +1,8 @@
-
+import  csv
+from bs4 import BeautifulSoup
+import lxml
+import requests
+page= requests.get("https://www.washingtonpost.com/technology/2020/09/25/privacy-check-blacklight/")
+def main (page):
+    src=page.content
+    soup=BeautifulSoup(src,'lxml')
